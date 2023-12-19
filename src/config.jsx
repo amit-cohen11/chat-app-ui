f85951956580b7aa93f6460ec03002e2fd712f07
amit cohen <cohen415@walla.com>
2023-12-19 23:33:45 +0200

    finished ui page

---
+++ src/config.jsx
@@ -0,0 +1,11 @@
+// let BASE_URL_SERVER = "";
+// if (process.env.NODE_ENV === "production") {
+//   BASE_URL_SERVER = "https://notes-app-server-hj12.onrender.com";
+// } else {
+// BASE_URL_SERVER = "http://localhost:8000";
+// }
+
+// export { BASE_URL_SERVER };
+
+const BASE_URL_SERVER = "http://localhost:5000";
+export { BASE_URL_SERVER };
