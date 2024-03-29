86cc0660e960e8d4064dbfb67f014023c10df428
amit cohen <cohen415@walla.com>
2024-03-29 14:57:00 +0300

    fixed git mistake

--- src/Context/chatProvider.jsx
+++ src/Context/chatProvider.jsx
@@ -12,7 +12,7 @@ const ChatProvider = ({ children }) => {
     const userInfo = JSON.parse(localStorage.getItem("userInfo"));
     setUser(userInfo);
 
-    if (!userInfo) {
+    if (userInfo === undefined) {
       history.push("/");
     }
   }, [history]);
