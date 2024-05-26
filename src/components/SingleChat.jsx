e54f6f1cc850425656516e7c7162cd13b7beac83
amit cohen <cohen415@walla.com>
2024-05-26 23:54:10 +0300

    finished update group modal ui

--- src/components/SingleChat.jsx
+++ src/components/SingleChat.jsx
@@ -5,6 +5,7 @@ import { ArrowBackIcon } from "@chakra-ui/icons";
 import { ChatState } from "../Context/chatProvider";
 import { getSender, getSenderFull } from "../config/ChatLogics";
 import ProfileModal from "./miscellaneous/ProfileModal";
+import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
   const { user, selectedChat, setSelectedChat } = ChatState();
@@ -36,13 +37,26 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
             ) : (
               <>
                 {selectedChat.chatName.toUpperCase()}
-                {/* <UpdateGroupChatModal
+                <UpdateGroupChatModal
                   fetchAgain={fetchAgain}
                   setFetchAgain={setFetchAgain}
-                /> */}
+                />
               </>
             )}
           </Text>
+          <Box
+            display="flex"
+            flexDir="column"
+            justifyContent="flex-end"
+            p={3}
+            bg="#E8E8E8"
+            w="100%"
+            h="100%"
+            borderRadius="lg"
+            overflowY="hidden"
+          >
+            Message Here
+          </Box>
         </>
       ) : (
         <Box display="flex" alignItems="center" justifyContent="center" h="100%">
---
+++ src/components/miscellaneous/UpdateGroupChatModal.jsx
@@ -0,0 +1,104 @@
+import {
+  ModalCloseButton,
+  ModalBody,
+  ModalHeader,
+  ModalContent,
+  ModalFooter,
+  ModalOverlay,
+  Button,
+  Modal,
+  useDisclosure,
+  IconButton,
+  useToast,
+  Box,
+  FormControl,
+  Input,
+} from "@chakra-ui/react";
+import { ViewIcon } from "@chakra-ui/icons";
+import { useState } from "react";
+
+import { ChatState } from "../../Context/chatProvider";
+import UserBadgeItem from "../UserAvatar/UserBadgeItem";
+
+const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { selectedChat, setSelectedChat, user } = ChatState();
+  const [groupChatName, setGroupChatName] = useState();
+  const [setsearch, setSetsearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [renameLoading, setRenameLoading] = useState(false);
+
+  const toast = useToast();
+
+  const handleRemove = () => {};
+
+  const handleRename = () => {};
+
+  const handleSearch = () => {};
+
+  return (
+    <>
+      <IconButton onClick={onOpen} display={{ base: "flex" }} icon={<ViewIcon />} />
+
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader
+            fontSize="35px"
+            fontFamily="Work sans"
+            display="flex"
+            justifyContent="center"
+          >
+            {selectedChat.chatName}
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Box w="100%" display="flex" flexWrap="wrap" pb={3}>
+              {selectedChat.users.map((u) => (
+                <UserBadgeItem
+                  key={user._id}
+                  user={u}
+                  handleFunction={() => handleRemove(u)}
+                />
+              ))}
+            </Box>
+
+            <FormControl display="flex">
+              <Input
+                placeholder="Chat Name"
+                mb={3}
+                value={groupChatName}
+                onChange={(e) => setGroupChatName(e.target.value)}
+              />
+              <Button
+                variant="solid"
+                colorScheme="teal"
+                ml={1}
+                isLoading={renameLoading}
+                onClick={handleRename}
+              >
+                Update
+              </Button>
+            </FormControl>
+            <FormControl display="flex">
+              <Input
+                placeholder="Add User to group"
+                mb={1}
+                onChange={(e) => handleSearch(e.target.value)}
+              />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="red" onClick={() => handleRemove(user)}>
+              Leave Group
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default UpdateGroupChatModal;
