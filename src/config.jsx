23a294f686148023e642704dc7311331f7001da6
amit cohen <cohen415@walla.com>
2024-06-17 23:25:29 +0300

    for deployment

--- src/config.jsx
+++ src/config.jsx
@@ -1,11 +1,8 @@
-// let BASE_URL_SERVER = "";
-// if (process.env.NODE_ENV === "production") {
-//   BASE_URL_SERVER = "https://notes-app-server-hj12.onrender.com";
-// } else {
-// BASE_URL_SERVER = "http://localhost:8000";
-// }
+let BASE_URL_SERVER = "";
+if (process.env.NODE_ENV === "production") {
+  BASE_URL_SERVER = "https://chat-app-server-0e9s.onrender.com";
+} else {
+  BASE_URL_SERVER = "http://localhost:5000";
+}
 
-// export { BASE_URL_SERVER };
-
-const BASE_URL_SERVER = "http://localhost:5000";
 export { BASE_URL_SERVER };
